89db45dc3f373d0197a830c07b4d82c1aaabfc13
Khoa Nguyen <khoa@redweb.dk>
2014-07-07 15:29:26 +0700

    Adding file info.php

---
+++ info.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package     RedITEM.Backend
+ * @subpackage  Template
+ *
+ * @copyright   Copyright (C) 2005 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+phpinfo();
